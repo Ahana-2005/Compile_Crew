d9c6d52e85069b3cbcc3f62a7a21c9de57aaa132
Ahana-2005 <ahana25052005@gmail.com>
2024-05-08 13:45:39 +0530

    .

--- P_01.c
+++ P_01.c
@@ -1,5 +1,18 @@
+/*Write a program in C to store elements in an array and print them.
+Test Data :
+Input 10 elements in the array :
+element - 0 : 1
+element - 1 : 1
+element - 2 : 2
+Expected Output :
+Elements in array are: 1 1 2 3 4 5 6 7 8 9*/
 #include <stdio.h>
 int main()
 {
-    printf("Hello");
+    int arr[10]={1,1,2,3,4,5,6,7,8,9};
+    printf("The elements in the array are:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("The elements in index %d of array is:%d\n",i,arr[i]);
+    }
 }
